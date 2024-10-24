abc7b80da13d25ff29c912eee251ad4123438236
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2024-10-24 12:18:58 -0300

    LPD-40087 Add ERC field to workflowDefinition

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
@@ -348,6 +348,7 @@ public class WorkflowDefinitionResourceImpl
 				setDateCreated(workflowDefinition::getCreateDate);
 				setDateModified(workflowDefinition::getModifiedDate);
 				setDescription(workflowDefinition::getDescription);
+				setExternalReferenceCode(workflowDefinition::getExternalReferenceCode);
 				setId(workflowDefinition::getWorkflowDefinitionId);
 				setName(workflowDefinition::getName);
 				setNodes(
