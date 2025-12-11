b4141855dda274c97bf08e3e1cff364caf2bd9d5
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-12-11 15:05:23 -0300

    LPD-65254 use correct groupId and insert at the right moment

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -515,9 +515,6 @@ public class ObjectEntryLocalServiceImpl
 				serviceContext.getAssetPriority(), serviceContext);
 		}
 
-		_addComments(
-			groupId, userId, objectDefinition, objectEntry, serviceContext);
-
 		_addFriendlyURLEntry(
 			objectDefinition, objectEntry, serviceContext, values);
 
@@ -527,6 +524,9 @@ public class ObjectEntryLocalServiceImpl
 
 			_startWorkflowInstance(userId, objectEntry, serviceContext, false);
 		}
+
+		_addComments(
+			groupId, userId, objectDefinition, objectEntry, serviceContext);
 
 		_updateResourcePermissions(
 			objectDefinition, objectEntry, serviceContext);
@@ -2582,6 +2582,10 @@ public class ObjectEntryLocalServiceImpl
 
 		if (!objectDefinition.isEnableComments() || (comments == null)) {
 			return;
+		}
+
+		if (groupId == 0) {
+			groupId = objectEntry.getNonzeroGroupId();
 		}
 
 		_discussionPermission.checkAddPermission(
