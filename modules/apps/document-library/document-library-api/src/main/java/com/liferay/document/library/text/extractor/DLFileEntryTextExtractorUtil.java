4f1c38c7ede20e834320665d896283cdba8f8ad9
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2026-08-11 16:45:38 -0300

    LPD-95804 move text extraction logic from service to api

---
+++ modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/text/extractor/DLFileEntryTextExtractorUtil.java
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.document.library.text.extractor;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileVersion;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Jhosseph Gonzalez
+ */
+public class DLFileEntryTextExtractorUtil {
+
+	public static String extractText(
+		DLFileEntry dlFileEntry, DLFileVersion dlFileVersion) {
+
+		DLFileEntryTextExtractor dlFileEntryTextExtractor =
+			_serviceTracker.getService();
+
+		if (dlFileEntryTextExtractor == null) {
+			throw new NullPointerException(
+				"DL file entry text extractor is null");
+		}
+
+		return dlFileEntryTextExtractor.extractText(dlFileEntry, dlFileVersion);
+	}
+
+	private static final ServiceTracker
+		<DLFileEntryTextExtractor, DLFileEntryTextExtractor> _serviceTracker;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(
+			DLFileEntryTextExtractorUtil.class);
+
+		ServiceTracker<DLFileEntryTextExtractor, DLFileEntryTextExtractor>
+			serviceTracker = new ServiceTracker<>(
+				bundle.getBundleContext(), DLFileEntryTextExtractor.class,
+				null);
+
+		serviceTracker.open();
+
+		_serviceTracker = serviceTracker;
+	}
+
+}
